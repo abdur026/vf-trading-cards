506cca918969925bbd7e414bd1c0de58ca68b6d4
hassanuahmad <ahmad12p@uwindsor.ca>
2023-06-02 17:54:42 -0400

    added max width to root page.js

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
             <html lang="en">
                 <body className={inter.className}>
                     <Navbar />
-                    {children}
+                    <div className="max-w-8xl mx-auto">{children}</div>
                 </body>
             </html>
         </>
