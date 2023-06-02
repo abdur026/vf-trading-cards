bbad7f07d1534d20b5754eca60e27d0271722bf1
hassanuahmad <ahmad12p@uwindsor.ca>
2023-06-02 17:53:31 -0400

    added navbar to the project and the routes

---
+++ app/deck-builder/page.js
@@ -0,0 +1,7 @@
+export default function DeckBuilder() {
+    return (
+        <div>
+            <h1>Deck Builder</h1>
+        </div>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,3 +1,7 @@
 export default function Home() {
-    return <h1>Welome to VF Trading Cards</h1>;
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
 }
