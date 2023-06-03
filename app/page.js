2bc4d22fd55999952650f753b7b3d5c754dfe02f
Abdur <abdur026@gmail.com>
2023-06-03 01:50:19 -0500

    updated indents

--- app/page.js
+++ app/page.js
@@ -1,31 +1,21 @@
-
 export default function Home() {
-    return (
+  return (
+    <div
+      style={{
+        backgroundImage: "url(background.png)",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column", // Added to stack elements vertically
+        justifyContent: "center",
+        alignItems: "center",
+        animation: "wave 5s infinite",
+      }}
+    >
+      <div class="main"></div>
 
-      <div
-        style={{
-          backgroundImage: "url(background.png)",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column", // Added to stack elements vertically
-          justifyContent: "center",
-          alignItems: "center",
-          animation: "wave 5s infinite",
-        }}
-      >
-      
-
-<div class="main">
-</div>
-  
-        <h1>Welcome to VF Trading Cards</h1>
-      </div>
-    );
-  }
-  
-  
-  
-  
-
+      <h1>Welcome to VF Trading Cards</h1>
+    </div>
+  );
+}
