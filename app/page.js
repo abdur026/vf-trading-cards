458063714ec3b74173b417afe4a38116b58dd83f
Abdur <abdur026@gmail.com>
2023-06-03 01:32:06 -0500

    Background done

--- app/page.js
+++ app/page.js
@@ -1,7 +1,31 @@
+
 export default function Home() {
     return (
-        <div>
-            <h1>Abdur</h1>
-        </div>
+
+      <div
+        style={{
+          backgroundImage: "url(background.png)",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column", // Added to stack elements vertically
+          justifyContent: "center",
+          alignItems: "center",
+          animation: "wave 5s infinite",
+        }}
+      >
+      
+
+<div class="main">
+</div>
+  
+        <h1>Welcome to VF Trading Cards</h1>
+      </div>
     );
-}
+  }
+  
+  
+  
+  
+
