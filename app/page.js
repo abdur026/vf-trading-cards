68e20bf2c1ea7c48670f32a55a59ae84ee95f243
Abdur <abdur026@gmail.com>
2023-06-05 19:08:22 -0500

    cards

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+
 export default function Home() {
   return (
     <div
@@ -14,8 +15,50 @@ export default function Home() {
       }}
     >
       <div class="main"></div>
+      <div>
+     
+         <App/>
+       
+      </div>
 
-      <h1>Welcome to VF Trading Cards</h1>
     </div>
+
+
   );
 }
+
+const Card = ({ image }) => {
+  return (
+    <div className="card">
+      <img src={image} alt="Card" className="w-32 h-auto" />
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="flex justify-start overflow-x-hidden gap-4">
+      <Card image="/images/1.png" />
+      <Card image="/images/2.png" />
+      <Card image="/images/4.png" />
+      <Card image="/images/5.png" />
+      <Card image="/images/6.png" />
+      <Card image="/images/7.png" />
+      <Card image="/images/9.png" />
+      <Card image="/images/10.png" />
+      <Card image="/images/11.png" />
+      <Card image="/images/12.png" />
+     
+    </div>
+  );
+};
+
+
+
+
+
+
+ 
+
+
+
